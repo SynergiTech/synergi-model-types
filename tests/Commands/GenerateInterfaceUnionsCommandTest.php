90fadfae5722b57b4f9b2d064487bfa6d601b3e2
Charley Reeson <neon.fortytwo@gmail.com>
2025-09-26 19:34:12 +0100

    .

--- tests/Commands/GenerateInterfaceUnionsCommandTest.php
+++ tests/Commands/GenerateInterfaceUnionsCommandTest.php
@@ -4,18 +4,17 @@ namespace SynergiTech\ExportTypes\Tests\Commands;
 
 use SynergiTech\ExportTypes\Tests\TestCase;
 
-// use Illuminate\Testing\PendingCommand;
-// use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Illuminate\Testing\PendingCommand;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
 class GenerateInterfaceUnionsCommandTest extends TestCase
 {
     public function testCommandExports(): void
-    {
-        $this->markTestSkipped('Requires implementation.');
-       /*  $command = $this->artisan('laravel-magic-enums:generate --input=app/Enums --output=enums');
+    { 
+        $command = $this->artisan('export-interface-unions:generate:generate --input=app/models --output=models');
         $this->assertInstanceOf(PendingCommand::class, $command);
         $result = $command->run();
         $this->assertSame(SymfonyCommand::SUCCESS, $result);
-        $this->assertFileExists('./enums/index.js'); */
+        $this->assertFileExists('./models/index.ts'); 
     }
 }
